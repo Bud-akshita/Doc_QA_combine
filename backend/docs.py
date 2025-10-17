20e977babad7568aa6199153773fb5a7a720e3cf
Bud-akshita <budgetalizer.e@gmail.com>
2025-10-17 15:50:02 +0530

    summary

--- backend/docs.py
+++ backend/docs.py
@@ -623,7 +623,7 @@ async def generate_summary(filename :str = Form(...),document_type: str = Form(.
             query_emb = embedding_model.embed_query(questions)
             best_chunks = retrieve_best_chunks(index, query_emb, docs, k=12, efSearch=16, space='cosine')
             context = "\n".join([chunk.page_content for chunk in best_chunks])
-            prompt.format(context=context, title =title, input=question)
+            prompt.format(context=context, title =title, input=questions)
             response = llm.invoke(final_prompt)
             result = response.content
             answer = "\n".join(result)
