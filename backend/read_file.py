daa749e90416c6d89b48f650212c3bb037251436
Bud-akshita <budgetalizer.e@gmail.com>
2025-10-25 16:24:09 +0530

    changed oem

--- backend/read_file.py
+++ backend/read_file.py
@@ -39,7 +39,7 @@ def process_pdf_page(page_num, page):
     else:
         pix = page.get_pixmap(dpi=300)
         image = Image.open(io.BytesIO(pix.tobytes("png")))
-        text = pytesseract.image_to_string(image)
+        text = pytesseract.image_to_string(image, config="--oem 1")
         return page_num, text.strip()
 
 def extract_content(file_path):
